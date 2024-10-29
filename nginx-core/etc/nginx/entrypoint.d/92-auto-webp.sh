874929485f70a92b57cd23352602508b148d65d7
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-30 00:11:52 +0330

    feat(nginx-core): remove auto webp configuration base on NGINX_AUTO_WEBP env

---
+++ nginx-core/etc/nginx/entrypoint.d/92-auto-webp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -eu
+
+ME=$(basename "$0")
+
+test -n "${NGINX_AUTO_WEBP:-}" && exit 0
+
+echo "$ME: Remove auto webp config"
+rm -fv /etc/nginx/conf.d/http.d/42-map-webp.conf
+rm -fv /etc/nginx/location.d/60-webp.conf
+
+exit 0
