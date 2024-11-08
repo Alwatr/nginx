b5ab60980e26e3230642731d6f960e81ddb29bd3
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-11-08 10:10:48 +0330

    chore(nginx-core): update CORS config removal path

--- nginx-core/etc/nginx/entrypoint.d/93-cors.sh
+++ nginx-core/etc/nginx/entrypoint.d/93-cors.sh
@@ -6,6 +6,6 @@ ME=$(basename "$0")
 test -n "${NGINX_CORS_ENABLE:-}" && exit 0
 
 echo "$ME: Remove CORS config"
-rm -fv /etc/nginx/conf.d/http.d/70-cors.conf
+rm -fv /etc/nginx/conf.d/location.d/70-cors.conf
 
 exit 0
