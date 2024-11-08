e7ab684ab37cb3760e419f6079e0c997adf21aa4
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-11-08 11:06:50 +0330

    chore(nginx-core): skip config removal in TEST_MODE for domain, webp, and CORS scripts

--- nginx-core/etc/nginx/entrypoint.d/91-force-domain.sh
+++ nginx-core/etc/nginx/entrypoint.d/91-force-domain.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -eu
 
-ME=$(basename "$0")
-
+test -n "${TEST_MODE:-}" && exit 0
 test -n "${NGINX_FORCE_DOMAIN:-}" && exit 0
 
+ME=$(basename "$0")
 echo "$ME: Remove force domain location config"
 rm -fv /etc/nginx/conf.d/location.d/40-force-domain.conf
 
--- nginx-core/etc/nginx/entrypoint.d/93-cors.sh
+++ nginx-core/etc/nginx/entrypoint.d/93-cors.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -eu
 
-ME=$(basename "$0")
-
+test -n "${TEST_MODE:-}" && exit 0
 test -n "${NGINX_CORS_ENABLE:-}" && exit 0
 
+ME=$(basename "$0")
 echo "$ME: Remove CORS config"
 rm -fv /etc/nginx/conf.d/location.d/70-cors.conf
 
