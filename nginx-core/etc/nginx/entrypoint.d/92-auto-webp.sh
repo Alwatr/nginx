302276ccf98a0cd8eb9378bf2fffa6f4f5d8796d
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-11-09 16:19:20 +0330

    chore(nginx-core): reorganize configuration files and remove deprecated settings

--- nginx-core/etc/nginx/entrypoint.d/92-auto-webp.sh
+++ nginx-core/etc/nginx/entrypoint.d/92-auto-webp.sh
@@ -7,6 +7,6 @@ test -n "${NGINX_AUTO_WEBP:-}" && exit 0
 ME=$(basename "$0")
 echo "$ME: Remove auto webp config"
 rm -fv /etc/nginx/conf.d/http.d/42-map-webp.conf
-rm -fv /etc/nginx/conf.d/location.d/60-webp.conf
+rm -fv /etc/nginx/conf.d/location.d/50-webp.conf
 
 exit 0
--- nginx-core/etc/nginx/entrypoint.d/93-cors.sh
+++ nginx-core/etc/nginx/entrypoint.d/93-cors.sh
@@ -6,6 +6,6 @@ test -n "${NGINX_CORS_ENABLE:-}" && exit 0
 
 ME=$(basename "$0")
 echo "$ME: Remove CORS config"
-rm -fv /etc/nginx/conf.d/location.d/70-cors.conf
+rm -fv /etc/nginx/conf.d/location.d/root.d/10-cors.conf
 
 exit 0
