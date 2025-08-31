e0431d352b136da568afe60387252277a7b28fa0
Ali Mihandoost <ali.mihandoost@gmail.com>
2025-08-31 16:51:34 +0330

    fix: refactor auto webp script to use case statement for NGINX_AUTO_WEBP variable

--- nginx-core/etc/nginx/entrypoint.d/92-auto-webp.sh
+++ nginx-core/etc/nginx/entrypoint.d/92-auto-webp.sh
@@ -2,11 +2,19 @@
 set -eu
 
 test -n "${TEST_MODE:-}" && exit 0
-test -n "${NGINX_AUTO_WEBP:-}" && exit 0
 
 ME=$(basename "$0")
-echo "$ME: Remove auto webp config"
-rm -fv /etc/nginx/conf.d/http.d/42-map-webp.conf
-rm -fv /etc/nginx/conf.d/location.d/50-webp.conf
+
+case "${NGINX_AUTO_WEBP:-}" in
+  1|true|yes|Yes|on|ON|True|TRUE)
+    echo "$ME: Enable auto webp config"
+    # keep the file
+    ;;
+  *)
+    echo "$ME: Remove auto webp config"
+    rm -fv /etc/nginx/conf.d/http.d/42-map-webp.conf
+    rm -fv /etc/nginx/conf.d/location.d/50-webp.conf
+    ;;
+esac
 
 exit 0
